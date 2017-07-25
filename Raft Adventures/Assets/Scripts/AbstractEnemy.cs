56e6819355d915b1b15ef188817c5184d4b0739f
WIN\AIT-UNF-GDC <teknisk@game.unf.dk>
2017-07-25 23:02:29 +0200

    shadowcasting

--- Raft Adventures/Assets/Scripts/AbstractEnemy.cs
+++ Raft Adventures/Assets/Scripts/AbstractEnemy.cs
@@ -3,11 +3,10 @@ using System.Collections;
 
 public abstract class AbstractEnemy : MonoBehaviour {
     public abstract void Intro(float posX, float posZ);
-
-	private bool isHitting=false;
+	public GameObject ShadowMaker;
 	public float Mass {
 		get {
-			if (Physics.Raycast(transform.position, new Vector3(0, -1, 0), transform.lossyScale.y / 2 + 1f, 1 << LayerMask.NameToLayer("Enemies") | 1 << LayerMask.NameToLayer("Default"))) {
+			if (Physics.Raycast(transform.position, new Vector3(0, -1, 0), transform.lossyScale.y / 2 + 1f, 1 << LayerMask.NameToLayer("Enemies") | 1 << LayerMask.NameToLayer("Raft"))) {
 				return GetComponent<Rigidbody>().mass;
 			} else {
 				return 0;
@@ -19,11 +18,17 @@ public abstract class AbstractEnemy : MonoBehaviour {
 		Destroy(gameObject,0.1f);
 	}
 
-	void OnCollisionEnter(Collision hit) {
-		isHitting = true;
+	void Start() { //write code to initialize an copy of this object on the raft, and set mesh rendere shadows to only shadows via recursion: http://answers.unity3d.com/questions/205391/how-to-get-list-of-child-game-objects.html
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, new Vector3(0, -1, 0), out hit, 20, 1 << LayerMask.NameToLayer("Default"))) {
+
+			//should be changed to object radius
+
+		}
 	}
-	void OnCollisionExit(Collision hit) {
-		isHitting = false;
+
+	void Update() {
+
 	}
 }
 
--- Raft Adventures/Assets/Scripts/StoneHitboxScript.cs
+++ Raft Adventures/Assets/Scripts/StoneHitboxScript.cs
@@ -3,15 +3,7 @@ using System.Collections;
 using System;
 
 public class StoneHitboxScript : AbstractEnemy {
-    // Use this for initialization
-    public void Start () {
-	}
-
     // Update is called once per frame
-    public void Update () {
-	
-	}
-
     public override void Intro(float posX,float posZ){
         transform.position=new Vector3(posX, 20, posZ);
     }
