093493fd70e6bc89606b4a66daa482fc70bdf5d2
WIN\AIT-UNF-GDC <teknisk@game.unf.dk>
2017-07-27 13:34:54 +0200

    menu music and scorscreen revert

---
+++ Raft Adventures/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour {
+
+	public Button myButton;
+
+	// Use this for initialization
+	void Start() {
+		myButton.onClick.AddListener(StartGame);
+	}
+	public void StartGame() {
+		SceneManager.LoadScene("Main scene");
+	}
+}
+
---
+++ Raft Adventures/Assets/Scripts/Level1Music.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level1Music : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(gameObject);
+	}
+}
