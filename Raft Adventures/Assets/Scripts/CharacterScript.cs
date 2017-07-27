a5cf0e995411b85ac16d412798880f170b21c02d
WIN\AIT-UNF-GDC <teknisk@game.unf.dk>
2017-07-27 17:53:20 +0200

    Virker

--- Raft Adventures/Assets/Scripts/CharacterScript.cs
+++ Raft Adventures/Assets/Scripts/CharacterScript.cs
@@ -7,22 +7,17 @@ public class CharacterScript : MonoBehaviour
     //start of changeble values from unity. Remember to reset
     public float moveSpeed = 2;
     private Rigidbody thisRigidbody;
+	private Animator ModelAnimator;
+	public float crossfadeTime = 0.1f;
 
     void Awake()
     {
         thisRigidbody = GetComponent<Rigidbody>();
+		ModelAnimator = GameObject.Find("Charactor").GetComponent<Animator>();
     }
 
     // Use this for initialization
-    void Start()
-    {
-
-    }
 	// Update is called once per frame
-	void Update()
-    {
-
-    }
 
     void FixedUpdate()
     {
@@ -33,7 +28,9 @@ public class CharacterScript : MonoBehaviour
 		thisRigidbody.velocity = movement;
 		if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) {
 			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * moveSpeed), 0.15f);
-
+			ModelAnimator.CrossFade("Run", crossfadeTime);
+		} else {
+			ModelAnimator.CrossFade("Idle", crossfadeTime);
 		}
 	}
 }
